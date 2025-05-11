38b8eca4a824e848daf28675b1e607e90ed5412c
Mathias Rendon <mathiassaid7@gmail.com>
2025-05-11 12:00:31 -0500

    refactor(db): optimize SQL table definitions for clarity and consistency
    Refactor table definitions in db.sql to improve readability and maintain consistency. Changes include:
    - Consolidate `AUTOINCREMENT` and `PRIMARY KEY` declarations on a single line.
    - Replace `TEXT` with `VARCHAR` for `nombre` and `descripcion` columns to better reflect data type constraints.
    - Adjust default values and column definitions for `tareas_completadas` to align with its purpose.

--- src/db/db.sql
+++ src/db/db.sql
@@ -1,20 +1,18 @@
 -- - Dos tablas básicas para la app de tasks.dev
 
 CREATE TABLE tareas (
-    id INTEGER PRIMARY KEY
-    AUTOINCREMENT,
-    nombre TEXT NOT NULL,
-    descripcion TEXT,
-    completada BOOLEAN NOT NULL DEFAULT false
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre VARCHAR(256) NOT NULL,
+    descripcion VARCHAR,
+    completada BOOLEAN NOT NULL DEFAULT false,
     hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 
 CREATE TABLE tareas_completadas (
-    id INTEGER PRIMARY KEY
-    AUTOINCREMENT,
-    nombre TEXT NOT NULL
-    descripcion TEXT,
-    completada BOOLEAN NOT NULL DEFAULT false
-    hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre VARCHAR(256) NOT NULL,
+    descripcion VARCHAR,
+    completada BOOLEAN NOT NULL DEFAULT true,
+    hora TIMESTAMP,
     hora_completada TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
